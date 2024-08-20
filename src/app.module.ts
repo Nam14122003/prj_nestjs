d2ac6d3d9ea71b6c46e4e70f9664b2ae0bf083ed
Nam14122003 <147422976+Nam14122003@users.noreply.github.com>
2024-08-20 11:28:23 +0700

    Create project and setup migration

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptios } from 'db/data-source';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [TypeOrmModule.forRoot(dataSourceOptios), UserModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
